5875d2b3a9f7470a96f35038c712fa361d1d8e1b
Dhruv Sharma <dhruvsh704@gmail.com>
2026-01-31 19:28:09 -0400

    Initial code

--- Scripts/Base.gd
+++ Scripts/Base.gd
@@ -3,9 +3,20 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	InitVariables();
+	SetupScreen();
+	SpawnPlayers();
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func InitVariables():
+	pass
+
+func SetupScreen():
+	pass
+
+func SpawnPlayers():
 	pass
