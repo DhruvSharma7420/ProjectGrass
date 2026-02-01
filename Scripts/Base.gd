58d52b4537ec787e25a2bc4954fa90dd56082b0f
Dhruv Sharma <dhruvsh704@gmail.com>
2026-02-01 00:22:52 -0400

    Majority of Car Control Logic

--- Scripts/Base.gd
+++ Scripts/Base.gd
@@ -1,22 +1,4 @@
-extends Node
+extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	InitVariables();
-	SetupScreen();
-	SpawnPlayers();
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func InitVariables():
-	pass
-
-func SetupScreen():
-	pass
-
-func SpawnPlayers():
-	pass
+const player1_camera_scene = preload("res://Scenes/player1.tscn")
+const player2_camera_scene = preload("res://Scenes/player2.tscn")
